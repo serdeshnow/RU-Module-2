1008c9b11f932d5203237d922d4c3deb6cc8dfed
serdeshnow <vserdes1977@mail.ru>
2024-07-24 23:19:42 +0300

    upd: Task-1 part-1

---
+++ Chapter-1/README.md
@@ -0,0 +1,57 @@
+## Project Setting. Installing
+
+npx create-react-app app
+
+> > cd app
+> > npm start
+
+## Project Setting. EditorConfig
+
+# [https://editorconfig.org/]
+
+# ./.editorconfig :
+
+root = true
+
+[*]
+end_of_line = lf
+insert_final_newline = true
+trim_trailing_whitespace = true
+charset = utf-8
+indent_style = tab
+indent_size = 2
+
+## Project setting. Package.json
+
+# ./app/package.json :
+
+"eslintConfig": {
+"extends": [
+"react-app",
+"react-app/jest",
+
+<!-- Install npm i eslint-config-prettier eslint-plugin-prettier prettier -->
+
+"prettier"
+],
+"plugins": [
+"prettier"
+]
+},
+
+## Project setting. Prettier
+
+# [https://prettier.io/docs/en/options]
+
+# ./app/.prettierrc.json :
+
+{
+"printWidth": 90,
+"tabWidth": 2,
+"useTabs": true,
+"semi": true,
+"singleQuote": false,
+"trailingComma": "all"
+}
+
+## Making project template.
--- Chapter-1/app/src/App.js
+++ Chapter-1/app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// Все остальное вроде бы Императивно написано...
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import logo from "./logo.svg";
+import "./App.css";
+// Декларативный стиль - импорт файлов
 
-export default App;
+export const App = () => {
+	const time = new Date();
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload2.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>Actual year: {time.getFullYear()}</p>
+				{/* Декларативный стиль - получаем год, неважно, как он считается под капотом  */}
+			</header>
+		</div>
+	);
+};
