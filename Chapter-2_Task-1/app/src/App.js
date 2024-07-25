8f51bda277a0aa40c3617afcc74977fda16ffb0a
serdeshnow <vserdes1977@mail.ru>
2024-07-25 19:00:42 +0300

    upd: Task-2 with unique key error

--- Chapter-2_Task-1/app/src/App.js
+++ Chapter-2_Task-1/app/src/App.js
@@ -1,83 +1,91 @@
 import styles from "./styles/app.module.css";
+import data from "./data.json";
 import { useState } from "react";
 
-function App() {
-	const [value, setValue] = useState("");
-	const [list, setList] = useState([]);
-	const [error, setError] = useState("");
+export const App = () => {
+	// Можно задать 2 состояния — steps и activeIndex
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(1);
+	// console.log(steps);
 
-	let isValueValid = value.length < 3 ? false : true;
+	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
 
-	const onInputButtonClick = () => {
-		const promptValue = prompt();
-		if (promptValue.length >= 3) {
-			setValue(promptValue);
-			setError("");
-		} else {
-			setError(
-				<div className={styles.error}>
-					Введенное значение должно содержать минимум 3 символа
-				</div>,
-			);
-		}
+	const onPrevClick = () => {
+		console.log("prev clicked");
+		setActiveIndex(activeIndex - 1);
 	};
 
-	const onAddButtonClick = () => {
-		if (value.length >= 3) {
-			const createdTime = new Date();
-			const updatedList = [
-				...list,
-				{
-					id: Date.now(),
-					value: `${value} - ${createdTime.toString().substring(3, 24)}`,
-				},
-			];
-			setList(updatedList);
-			setValue("");
-			setError("");
-		}
-		console.log(list);
+	const onNextClick = () => {
+		console.log("next clicked");
+
+		setActiveIndex(activeIndex + 1);
 	};
+
+	const onRepeatClick = () => {
+		console.log("repeat clicked");
+		setActiveIndex(0);
+	};
+
+	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+
+	let isFirstStep = activeIndex === 0;
+	let isLastStep = activeIndex === steps.length - 1;
 
 	return (
-		<div className={styles.app}>
-			<h1 className={styles["page-heading"]}>Ввод значения</h1>
-			<p className={styles["no-margin-text"]}>
-				Текущее значение <code>value</code>: "
-				<output className={styles["current-value"]}>{value}</output>"
-			</p>
-			{error !== "" && error}
-			<div className={styles["button-container"]}>
-				<button className={styles.button} onClick={onInputButtonClick}>
-					Ввести новое
-				</button>
-				<button
-					className={styles.button}
-					disabled={!isValueValid}
-					onClick={onAddButtonClick}
-				>
-					Добавить в список
-				</button>
-			</div>
-			<div className={styles["list-container"]}>
-				<h2 className={styles["list-heading"]}>Список:</h2>
-
-				<ul className={styles.list}>
-					{list.length !== 0 ? (
-						list.map(({ id, value }) => (
-							<li className={styles["list-item"]} key={id}>
-								{value}
-							</li>
-						))
-					) : (
-						<p className={styles["no-margin-text"]}>
-							Нет добавленных элементов
-						</p>
-					)}
-				</ul>
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles["steps-content"]} key={steps[activeIndex].id}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles["steps-list"]}>
+						{data &&
+							data.map((item, index) => (
+								<>
+									<li
+										className={
+											styles["steps-item"] +
+											(activeIndex + 1 === Number(item.id)
+												? " " + styles.active
+												: "") +
+											(activeIndex + 1 > Number(item.id)
+												? " " + styles.done
+												: "")
+										}
+										key={Number(item.id)}
+									>
+										<button
+											className={styles["steps-item-button"]}
+											onClick={() => setActiveIndex(Number(item.id) - 1)}
+										>
+											{index + 1}
+										</button>
+										{item.title}
+									</li>
+								</>
+							))}
+					</ul>
+					<div className={styles["buttons-container"]}>
+						<button
+							className={styles.button}
+							onClick={onPrevClick}
+							disabled={isFirstStep}
+						>
+							Назад
+						</button>
+						{!isLastStep ? (
+							<button className={styles.button} onClick={onNextClick}>
+								Далее
+							</button>
+						) : (
+							<button className={styles.button} onClick={onRepeatClick}>
+								Начать сначала
+							</button>
+						)}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
-}
-
-export default App;
+};
--- Chapter-2_Task-1/app/src/index.js
+++ Chapter-2_Task-1/app/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import App from "./App.js";
+import { App } from "./app.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
 	<React.StrictMode>
 		<App />
