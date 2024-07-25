562ebe6aa0dc574f4664b080b12a30616c1d0dfe
serdeshnow <vserdes1977@mail.ru>
2024-07-25 14:27:45 +0300

    upd: clean before Video-7

--- Chapter-1/app/src/App.js
+++ Chapter-1/app/src/App.js
@@ -1,26 +1,11 @@
-// Все остальное вроде бы Императивно написано...
-
 import logo from "./logo.svg";
 import "./App.css";
-// import { createElement } from "react";
-import { MyComponent } from "./MyComponent";
-// Декларативный стиль - импорт файлов
 
 export const App = () => {
-	// const time = new Date();
-
-	// 6. Responding on events & conditional rendering
-	const onClick = () => {
-		console.log(123);
-	};
-
 	return (
 		<div className="App">
-			<header className="App-header" onClick={onClick}>
+			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload2.
-				</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
@@ -29,10 +14,6 @@ export const App = () => {
 				>
 					Learn React
 				</a>
-				{/* <p>Actual year: {time.getFullYear()}</p> */}
-				{/* Декларативный стиль - получаем год, неважно, как он считается под капотом  */}
-				{/* <MyComponent type="normal" value="123" /> */}
-				<MyComponent />
 			</header>
 		</div>
 	);
